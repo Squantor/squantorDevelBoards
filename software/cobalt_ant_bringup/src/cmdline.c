62b46f4e17c6286fd7460d5474756f8ff12accd7
Bart Bilos <anton.bilos@gmail.com>
2018-01-08 23:14:51 +0100

    * completely switched over to sqstrstol for the cmdline argument parsing

--- software/cobalt_ant_bringup/src/cmdline.c
+++ software/cobalt_ant_bringup/src/cmdline.c
@@ -24,6 +24,7 @@ SOFTWARE.
 
 #include <string.h>
 #include <sqstring.h>
+#include <sqstdlib.h>
 
 #include "results.h"
 #include "cmdline.h"
@@ -32,90 +33,6 @@ SOFTWARE.
 #define	ASCII_BS	(8)	// backspace
 #define ASCII_SPACE	(32)	// space
 #define	ASCII_CR	(13)	// Carriage Return
-
-result cmdlineParseInt(char * token, int * value)
-{
-    int tokenLength = sqstrlen(token);
-    int multiplier = 1;
-    int i = tokenLength;
-    int parsedValue = 0;
-    while(i != 0)
-    {
-        i--;
-        switch(token[i])
-        {
-            case '0':  break;
-            case '1': parsedValue = parsedValue + (1 * multiplier); break;
-            case '2': parsedValue = parsedValue + (2 * multiplier); break;
-            case '3': parsedValue = parsedValue + (3 * multiplier); break;
-            case '4': parsedValue = parsedValue + (4 * multiplier); break;
-            case '5': parsedValue = parsedValue + (5 * multiplier); break;
-            case '6': parsedValue = parsedValue + (6 * multiplier); break;
-            case '7': parsedValue = parsedValue + (7 * multiplier); break;
-            case '8': parsedValue = parsedValue + (8 * multiplier); break;
-            case '9': parsedValue = parsedValue + (9 * multiplier); break;
-            case '-': parsedValue = ~parsedValue; 
-                // decrement by one due to twos complement difference
-                *value = parsedValue+1;
-                return noError;
-                break;
-            default: return cmdlineInvalidArg;
-        }
-        multiplier *= 10;
-    }
-    *value = parsedValue;
-    return noError;
-}
-
-result cmdlineParseHex(char * token, int * value)
-{
-    int tokenLength = sqstrlen(token);
-    unsigned int multiplier = 1;
-    int i = tokenLength;
-    unsigned parsedValue = 0;
-    while(i != 2)
-    {
-        i--;
-        switch(token[i])
-        {
-            case '0':  break;
-            case '1': parsedValue = parsedValue + (0x1 * multiplier); break;
-            case '2': parsedValue = parsedValue + (0x2 * multiplier); break;
-            case '3': parsedValue = parsedValue + (0x3 * multiplier); break;
-            case '4': parsedValue = parsedValue + (0x4 * multiplier); break;
-            case '5': parsedValue = parsedValue + (0x5 * multiplier); break;
-            case '6': parsedValue = parsedValue + (0x6 * multiplier); break;
-            case '7': parsedValue = parsedValue + (0x7 * multiplier); break;
-            case '8': parsedValue = parsedValue + (0x8 * multiplier); break;
-            case '9': parsedValue = parsedValue + (0x9 * multiplier); break;
-            case 'A': case 'a': parsedValue = parsedValue + (0xA * multiplier); break;
-            case 'B': case 'b': parsedValue = parsedValue + (0xB * multiplier); break;
-            case 'C': case 'c': parsedValue = parsedValue + (0xC * multiplier); break;
-            case 'D': case 'd': parsedValue = parsedValue + (0xD * multiplier); break;
-            case 'E': case 'e': parsedValue = parsedValue + (0xE * multiplier); break;
-            case 'F': case 'f': parsedValue = parsedValue + (0xF * multiplier); break;
-            default: return cmdlineInvalidArg;
-        }
-        multiplier = multiplier << 4;
-    }
-    *value = (int) parsedValue;
-    return noError;
-}
-
-result cmdlineParseArg(char * token, int * value)
-{
-    int tokLen = sqstrlen(token);
-    // if the token is smaller then 3 then it is not hex
-    if(tokLen > 2)
-    {
-        if((token[0] == '0') && (token[1] == 'x'))
-        {
-            return cmdlineParseHex(token, value);
-        }
-    }
-    // not hex, fall through to int parsing
-    return cmdlineParseInt(token, value);
-}
 
 result cmdlineParse(char * line)
 {
@@ -134,9 +51,7 @@ result cmdlineParse(char * line)
                 char *arg = strtok(NULL,STRTOK_DELIM);
                 if(arg == NULL)
                     return cmdlineInvalidArg;
-                result argparse = cmdlineParseArg(arg, &arguments[j]);
-                if(argparse != noError)
-                    return argparse;
+                arguments[j] = sqstrstol(arg);
             }
             // call the matched command with the argument count
             cmdLineEntries[i].argHandler(arguments);
@@ -169,7 +84,7 @@ void cmdlineProcess(void)
 		case ASCII_CR:
 			CHAR_PUT(ASCII_CR);
 			// terminate string
-			commandline[commandlineIndex+1] = 0;
+			commandline[commandlineIndex] = 0;
 			// call handler
 			cmdlineParse(commandline);
 			commandlineIndex = 0;
