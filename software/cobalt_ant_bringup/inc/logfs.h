502204d7b882bd5da28aefa6a8e0d5062be1ca6d
Bart Bilos <anton.bilos@gmail.com>
2018-03-03 20:55:43 +0100

    * updated to new logfs file structure with FAT

--- software/cobalt_ant_bringup/inc/logfs.h
+++ software/cobalt_ant_bringup/inc/logfs.h
@@ -5,11 +5,12 @@
 
 result fsInit();
 
-result fsFileCreate();
+result fsFileCreate(uint16_t fileId, uint32_t fileSize);
+result fsFileDelete(uint32_t fileId);
 // TODO File listing
 // first file in dir
 // next file in dir
-// TODO File deletion
+
 
 
 #endif
--- software/cobalt_ant_bringup/src/logfs.c
+++ software/cobalt_ant_bringup/src/logfs.c
@@ -3,51 +3,64 @@
 #include <results.h>
 #include <driver_W25Q32BV.h>
 
-#define	LOGFS_MAGIC_USED	(0xDEADBEEF)
-#define	LOGFS_MAGIC_EMPTY	(0xFFFFFFFF)
+#define	LOGFS_MAGIC_USED	(0xDEFF)
+#define	LOGFS_MAGIC_STALE	(0xDEAD)
+#define	LOGFS_MAGIC_EMPTY	(0xFFFF)
+#define MAX_INODES			(1024)
+
 
 #define	LOGFS_NEXTSECTADDR(address) ((((address) + 4096) & 0xFFFFF000))
 
 typedef struct
 {
-	uint32_t magic;
-	uint32_t id;
+	uint16_t magic;
+	uint16_t flags;
+	uint32_t location;
 	uint32_t size;
-	uint32_t flags;
+	uint16_t id;
+	uint16_t checksum;	// TODO
 } fsINode;
 
-uint32_t freeSpaceLocation;
+uint32_t freeSpaceLocation;	// point to the start of free space
+uint32_t nextEntry;		// point to the next empty entry in the inode table
 
 
 result fsInit()
 {
-	freeSpaceLocation = 0;
 	// init the housekeeping structures
-	// walk through flash to find free space
+	freeSpaceLocation = 0;
+	nextEntry = 0;
+
+	// walk through flash to find a free space block
 	fsINode file;
-	flashRead(freeSpaceLocation, &file, sizeof(file));
-	while(file.magic == LOGFS_MAGIC_USED)
+	flashRead(nextEntry, &file, sizeof(file));
+	while(file.magic == LOGFS_MAGIC_USED && (nextEntry < (MAX_INODES*sizeof(fsINode))))
 	{
-		// add 4096 for the inode sector
-		freeSpaceLocation += LOGFS_NEXTSECTADDR(file.size + 4096) ;
-		flashRead(freeSpaceLocation, &file, sizeof(file));
+		freeSpaceLocation = LOGFS_NEXTSECTADDR(file.size + file.location);
+		nextEntry += sizeof(fsINode);
 	}
+	return noError;
+}
+
+result fsFileCreate(uint16_t fileId, uint32_t fileSize)
+{
+	fsINode newFile;
+	newFile.magic = LOGFS_MAGIC_USED;
+	newFile.flags = 0;
+	newFile.location = freeSpaceLocation;
+	newFile.size = fileSize;
+	newFile.id = fileId;
+
+	flashWrite(nextEntry, &newFile, sizeof(newFile));
+
+	// add 4096 for the inode sector
+	freeSpaceLocation = LOGFS_NEXTSECTADDR(freeSpaceLocation + fileSize + 4096);
+	nextEntry += sizeof(fsINode);
 
 	return noError;
 }
 
-result fsFileCreate(uint32_t fileId, uint32_t fileSize)
+result fsFileDelete(uint32_t fileId)
 {
-	fsINode newFile;
-	newFile.magic = LOGFS_MAGIC_USED;
-	newFile.id = fileId;
-	newFile.size = fileSize;
-	newFile.flags = 0;
-
-	flashWrite(freeSpaceLocation, &newFile, sizeof(newFile));
-
-	// add 4096 for the inode sector
-	freeSpaceLocation = LOGFS_NEXTSECTADDR(freeSpaceLocation + fileSize + 4096);
-
 	return noError;
 }
