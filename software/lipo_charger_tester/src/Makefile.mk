6911661c2f75dc67833864567363155ff7d4b127
Bart Bilos <squantmuts@gmail.com>
2019-03-01 09:57:01 +0100

    updated makefiles

--- software/lipo_charger_tester/src/Makefile.mk
+++ software/lipo_charger_tester/src/Makefile.mk
@@ -10,7 +10,7 @@ LIBDIR =
 LIBS =
 RLIBDIR = 
 DEFINES = -DCORE_M0PLUS -D__USE_ROMDIVIDE
+COMPILE_C_FLAGS = -mcpu=cortex-m0 -mthumb
 COMPILE_CXX_FLAGS = -mcpu=cortex-m0 -mthumb
-COMPILE_CPP_FLAGS = -mcpu=cortex-m0 -mthumb
 COMPILE_ASM_FLAGS =
 LINK_FLAGS = -mcpu=cortex-m0 -mthumb
