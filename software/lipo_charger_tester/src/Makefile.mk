a1f65041f7be82eeb362515b00b1a014bfc4e365
Bart Bilos <squantmuts@gmail.com>
2019-03-01 22:59:58 +0100

    WIP makefile integration integrating minilibc and other library frameworks into this project

--- software/lipo_charger_tester/src/Makefile.mk
+++ software/lipo_charger_tester/src/Makefile.mk
@@ -5,10 +5,11 @@ BOARD =
 C_SOURCES = 
 CXX_SOURCES = src/startup.cpp src/main.cpp src/board.cpp
 S_SOURCES = src/aeabi_romdiv_patch.s
-INCLUDES = -Iinc
+INCLUDES = -Iinc -I../lpc_chip_82x/inc -I../cortexM_startup_code/inc
 LIBDIR = 
-LIBS =
-RLIBDIR = 
+LIBS = -llpc_chip_82x
+RLIBDIR = -L"../lpc_chip_82x/bin/release"
+DLIBDIR = -L"../lpc_chip_82x/bin/debug"
 DEFINES = -DCORE_M0PLUS -D__USE_ROMDIVIDE
 COMPILE_C_FLAGS = -mcpu=cortex-m0 -mthumb
 COMPILE_CXX_FLAGS = -mcpu=cortex-m0 -mthumb
