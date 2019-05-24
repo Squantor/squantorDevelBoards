67b717c81274138de24f891a0f4d315eb88586db
Bart Bilos <squantmuts@gmail.com>
2019-05-24 11:17:12 +0200

    updated to newer libraries updated builds

--- software/lipo_charger_tester/src/Makefile.mk
+++ software/lipo_charger_tester/src/Makefile.mk
@@ -18,7 +18,7 @@ RLIBDIR = -L"../lpc_chip_82x/bin/release" \
 DLIBDIR = -L"../lpc_chip_82x/bin/debug" \
 -L"../squantorLibC/bin/CortexM0/debug/" \
 -L"../squantorLibEmbeddedC/bin/CortexM0/debug/"
-DEFINES = -DCORE_M0PLUS -D__USE_ROMDIVIDE
+DEFINES = -DCORE_M0PLUS -D__USE_ROMDIVIDE -DCHIP_LPC82X
 COMPILE_C_FLAGS = -std=gnu11 -Wall -Wextra -Werror -Wno-main -fno-common -c -fmessage-length=0 -fno-builtin -ffunction-sections -fdata-sections
 COMPILE_CXX_FLAGS = -std=c++17 -Wall -Wextra -Werror -Wno-main -fno-common -c -fmessage-length=0 -fno-builtin -ffunction-sections -fdata-sections -fno-rtti -fno-exceptions
 COMPILE_ASM_FLAGS = -c -x assembler-with-cpp
@@ -26,17 +26,17 @@ LINK_FLAGS = -nostdlib -Xlinker --gc-sections -Xlinker -print-memory-usage
 
 #custom build rules
 pre-clean:
-	$(MAKE) -C ../lpc_chip_82x clean
+	$(MAKE) -C ../lpc_chip_82x clean MCU=LPC82X
 	$(MAKE) -C ../squantorLibC clean
 	$(MAKE) -C ../squantorLibEmbeddedC clean
 
 pre-release:
-	$(MAKE) -C ../lpc_chip_82x release
+	$(MAKE) -C ../lpc_chip_82x release MCU=LPC82X
 	$(MAKE) -C ../squantorLibC release PLATFORM=CortexM0
 	$(MAKE) -C ../squantorLibEmbeddedC release PLATFORM=CortexM0
 
 pre-debug:
-	$(MAKE) -C ../lpc_chip_82x debug
+	$(MAKE) -C ../lpc_chip_82x debug MCU=LPC82X
 	$(MAKE) -C ../squantorLibC debug PLATFORM=CortexM0
 	$(MAKE) -C ../squantorLibEmbeddedC debug PLATFORM=CortexM0
 
