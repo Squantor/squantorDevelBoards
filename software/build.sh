3114915a03f7b1a7a44122632107f12de4706e6a
Squantor <squantmuts@gmail.com>
2020-12-20 23:15:44 +0100

    added visual studio code things, added gdb script, made debugging working

--- software/build.sh
+++ software/build.sh
@@ -3,7 +3,10 @@
 make -j18 PROJECT=PC CONFIG=release
 make -j18 PROJECT=PC CONFIG=debug
 make -j18 PROJECT=PC doc
-make -j18 PROJECT=LPC824 CONFIG=release
-make -j18 PROJECT=LPC824 CONFIG=debug
-make -j18 PROJECT=LPC824 doc
+make -j18 PROJECT=LPC824_template CONFIG=release
+make -j18 PROJECT=LPC824_template CONFIG=debug
+make -j18 PROJECT=LPC824_template doc
+make -j18 PROJECT=nuclone_LPC824M201JHI33_expansion_serial_flash CONFIG=release
+make -j18 PROJECT=nuclone_LPC824M201JHI33_expansion_serial_flash CONFIG=debug
+make -j18 PROJECT=nuclone_LPC824M201JHI33_expansion_serial_flash doc
 
