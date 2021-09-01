5ec07ae50c37a999521b4ffd6b20d8a3aeee58fa
Squantor <squantmuts@gmail.com>
2021-09-01 22:11:50 +0200

    updated build environment

--- software/targets/src/LPC84X.cpp
+++ software/targets/src/LPC84X.cpp
@@ -1,9 +1,9 @@
 /*
-SPDX-License-Identifier: MIT
-
-Copyright (c) 2021 Bart Bilos
-For conditions of distribution and use, see LICENSE file
-*/
+ * SPDX-License-Identifier: MIT
+ *
+ * Copyright (c) 2021 Bart Bilos
+ * For conditions of distribution and use, see LICENSE file
+ */
 
 #if defined (__cplusplus)
 extern "C" {
@@ -16,7 +16,7 @@ void Dummy_Handler(void);
 #endif
 
 #include "cortexm_irqs_lpc.cpp"
-#include "isr_LPC112X.cpp"
+#include "isr_lpc84X.cpp"
 
 void Dummy_Handler(void) {
     while (1);
